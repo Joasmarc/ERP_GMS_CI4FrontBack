cdd844618255fac27b4adf32ac2b18c327c7f908
Joasmarc <joasmarc@gmail.com>
2026-05-12 03:07:24 -0500

    Implementacion de tabla ciuadades

---
+++ app/Models/Cities.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Cities extends Model
+{
+    // 1.0 Configurar tabla de ciudades
+    protected $table = 'cities';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+
+    // 2.0 Configurar campos permitidos
+    protected $allowedFields = [
+        'name',
+        'code',
+        'state'
+    ];
+
+    // 3.0 Configurar tipos de datos
+    protected $castings = [
+        'id' => 'int',
+        'name' => 'string',
+        'code' => 'string',
+        'state' => 'string'
+    ];
+
+    // 4.0 Configurar Timestamps (deshabilitado por falta de columnas en BD)
+    protected $useTimestamps = false;
+
+    // 5.0 Validación de datos
+    protected $validationRules = [
+        'name' => 'required|string|max_length[55]',
+        'code' => 'required|string|max_length[10]',
+        'state' => 'required|in_list[ACTIVO,INACTIVO]'
+    ];
+}
